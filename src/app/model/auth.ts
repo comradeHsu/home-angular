ebce5133e69ef029ab0a22251a674510a87f8100
knockinHeavenDoor <61239289@qq.com>
2017-10-21 23:36:16 +0800

    创建相应的model

---
+++ src/app/model/auth.ts
@@ -0,0 +1,10 @@
+import {User} from './user';
+/**
+ * Created by Administrator on 2017/10/21.
+ */
+export class Auth {
+  user: User;
+  hasError: boolean;
+  errMsg: string;
+  redirectUrl: string;
+}
---
+++ src/app/model/user.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/10/21.
+ */
+export class User {
+  id: number;
+  username: string;
+  password: string;
+}
