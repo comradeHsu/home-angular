68b5eea662c6fda88dea93ec060e438597ef27b1
knockinHeavenDoor <61239289@qq.com>
2017-10-22 23:53:02 +0800

    创建admin相关组件

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminMenuComponent } from './admin-menu/admin-menu.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AdminMenuComponent]
+})
+export class AdminModule { }
