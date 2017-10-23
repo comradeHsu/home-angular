81b4741570462eb2a461c3af44b3928bbe4b3305
knockinHeavenDoor <61239289@qq.com>
2017-10-23 23:09:46 +0800

    创建组件

--- src/app/admin/admin-route.module.ts
+++ src/app/admin/admin-route.module.ts
@@ -1,6 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {AdminComponent} from './admin.component';
+import { AdminUserListComponent } from './admin-user-list/admin-user-list.component';
+import { AdminRentListComponent } from './admin-rent-list/admin-rent-list.component';
+import { AdminRentAddComponent } from './admin-rent-add/admin-rent-add.component';
+import { AdminHouseListComponent } from './admin-house-list/admin-house-list.component';
+import { AdminHouseAddComponent } from './admin-house-add/admin-house-add.component';
 /**
  * Created by Administrator on 2017/10/23.
  import { Routes } from "@angular/router/router";
@@ -16,6 +21,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [ RouterModule.forChild(routes) ],
-  exports: [ RouterModule ]
+  exports: [ RouterModule ],
+  declarations: [AdminUserListComponent, AdminRentListComponent, AdminRentAddComponent, AdminHouseListComponent, AdminHouseAddComponent]
 })
 export class AdminRouteModule { }
