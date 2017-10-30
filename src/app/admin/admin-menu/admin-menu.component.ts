c69ddc859ccd14862c06f05b5090ecda8b858b3e
knockinHeavenDoor <61239289@qq.com>
2017-10-30 23:16:55 +0800

    分别使用两种方式实现同一个ui交互

--- src/app/admin/admin-menu/admin-menu.component.ts
+++ src/app/admin/admin-menu/admin-menu.component.ts
@@ -15,9 +15,26 @@ export class AdminMenuComponent implements OnInit {
     iconRegistry.addSvgIcon(
       'down',
       sanitizer.bypassSecurityTrustResourceUrl('assets/img/down.svg'));
+    // const eles = ele.getElementsByClassName('able');
+    // for (let i = 0; i < eles.length; i++) {
+    //   eles.item(i).addEventListener('mouseover', data => {
+    //     const target = (<Element>event.target) || event.srcElement;
+    //     target.setAttribute('style', 'background-color:#6b9dae');
+    //   });
+    // }
   }
 
   ngOnInit() {
+    // angular中document代表本页面元素
+    const eles = document.getElementsByClassName('able');
+    for (let i = 0; i < eles.length; i++) {
+      eles.item(i).addEventListener('mouseover', data => {
+        eles.item(i).setAttribute('style', 'background-color:#6b9dae');
+      });
+      eles.item(i).addEventListener('mouseout', data => {
+        eles.item(i).setAttribute('style', 'background-color:#eef1f6');
+      });
+    }
   }
 
   toggle(index: number) {
@@ -29,12 +46,24 @@ export class AdminMenuComponent implements OnInit {
     this.hide[index] = !this.hide[index];
   }
 
-  mouseMove(item: Element) {
-    item.setAttribute('style', 'background-color:#6b9dae');
+  // mouseMove(item: Element) {
+  //   item.setAttribute('style', 'background-color:#6b9dae');
+  // }
+  //
+  // mouseOut(item: Element) {
+  //   item.setAttribute('style', 'background-color:#eef1f6');
+  // }
+
+  mouse(event: MouseEvent, ele: Element) {
+    const target = (<Element>event.target) || event.srcElement;
+    if (event.type === 'mouseover') {
+      ele.setAttribute('style', 'background-color:#6b9dae');
+    }
+    if (event.type === 'mouseout') {
+      ele.setAttribute('style', 'background-color:#eef1f6');
+    }
   }
 
-  mouseOut(item: Element) {
-    item.setAttribute('style', 'background-color:#eef1f6');
-  }
+
 
 }
