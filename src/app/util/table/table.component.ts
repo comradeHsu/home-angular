dfa7a911dc828ba0b059b7b0bcc4650b845fd42a
knockinHeavenDoor <61239289@qq.com>
2017-11-20 00:34:15 +0800

    点击选中问题尚未解决

--- src/app/util/table/table.component.ts
+++ src/app/util/table/table.component.ts
@@ -10,7 +10,20 @@ export class TableComponent implements OnInit {
   @Input() houses: RentHouse[];
   constructor() { }
 
+  checks: any[] = [];
+  indexs: number[] = [];
   ngOnInit() {
+    for (let index in this.houses) {
+      this.checks.push({checked:''});
+      console.log(index);
+      this.indexs.push(Number(index));
+    }
+  }
+
+  selected(index: number) {
+    console.log(index);
+    // this.checks[index].checked = 'checked';
+    // console.log(this.checks);
   }
 
 }
