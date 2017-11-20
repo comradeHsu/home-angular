338686211d2bf1e26215a2114890cc9759e48bdb
knockinHeavenDoor <61239289@qq.com>
2017-11-20 23:41:23 +0800

    Update status.pipe.ts
    本地不能修改，只好在线

--- src/app/pipe/status.pipe.ts
+++ src/app/pipe/status.pipe.ts
@@ -1,3 +1,24 @@
 /**
  * Created by Administrator on 2017/11/20.
  */
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({name: 'status'})
+export class StatusPipe implements PipeTransform {
+  transform(value: number): string {
+    let status: string;
+    switch(value) {
+      case 1 :
+        status = '已打电话';
+        break;
+      case 2 :
+        break;
+      case 3 :
+        break;
+      case 4 :
+        break;
+      default:
+        break;
+    }
+    return status;
+  }
+}
