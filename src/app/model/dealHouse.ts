52d859796de3355731d58c795d8bb9ebe87a052c
knockinHeavenDoor <61239289@qq.com>
2017-11-21 23:28:22 +0800

    创建买卖房子model

---
+++ src/app/model/dealHouse.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/11/21.
+ */
+export class DealHouse {
+  id: string;
+  totalPrice: string;
+  refPrice: string;
+  tradingRight: string;
+  equityYear: string;
+  buildYear: string;
+  images: string[];
+  checked: string;
+}
