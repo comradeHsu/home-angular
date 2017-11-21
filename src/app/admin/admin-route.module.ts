2807a9d349054b85d19f884b2a48be8d27700d41
knockinHeavenDoor <61239289@qq.com>
2017-11-21 23:56:10 +0800

    房产列表实现

--- src/app/admin/admin-route.module.ts
+++ src/app/admin/admin-route.module.ts
@@ -36,6 +36,10 @@ const routes: Routes = [
       {
         path: 'rentList',
         component: AdminRentListComponent
+      },
+      {
+        path: 'houseList',
+        component: AdminHouseListComponent
       }
     ]
   }
