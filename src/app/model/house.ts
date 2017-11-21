fef0d601ec9288eb0c6a1156a4d59f9283bd43f8
knockinHeavenDoor <61239289@qq.com>
2017-11-21 23:22:15 +0800

    重新构建model

---
+++ src/app/model/house.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2017/11/21.
+ */
+export class House {
+  isDeleted: string;
+  createBy: string;
+  updateBy: string;
+  createDate: Date;
+  updateDate: Date;
+  title: string;
+  name: string;
+  houseType: string;
+  houseOrientation: string;
+  floor: string;
+  decorate: string;
+  area: string;
+  addr: string;
+  phone: string;
+  status: string;
+  isPublic: string;
+  desc: string;
+  type: string;
+}
--- src/app/model/rentHouse.ts
+++ src/app/model/rentHouse.ts
@@ -1,22 +1,11 @@
+import {House} from './house';
 /**
  * Created by Administrator on 2017/11/19.
  */
-export class RentHouse {
-  title: string;
-  author?: string;
-  price?: number;
-  rentStyle?: string;
-  fangxiang?: string;
-  floor?: number;
-  huxing?: string;
-  zhuangxiu?: string;
-  mianji: number;
-  address: string;
-  fangdongPhone: number;
-  yewuyuanPhone?: number;
-  status: number;
-  isPublic?: string;
-  image?: string[];
-  desc?: string;
+export class RentHouse extends House {
+  id: string;
+  rentPrice: string;
+  rentMethod: string;
+  images: string[];
   checked: string;
 }
