d2da87beb88cad6f823db852d1e136c7cd004930
knockinHeavenDoor <61239289@qq.com>
2017-12-17 00:18:24 +0800

    实现注销登陆

--- src/app/admin/admin-header/admin-header.component.ts
+++ src/app/admin/admin-header/admin-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  loginOut() {
+    window.sessionStorage.clear();
+    this.router.navigate(['login']);
   }
 
 }
